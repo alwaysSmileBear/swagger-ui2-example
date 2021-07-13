e382398f86b788dbd44ddd149db0cecc3ee016c0
DESKTOP-KPD0ONM\Notitle <ponompo@gmail.com>
2021-07-13 21:33:47 +0900

    #프로젝트 생성

---
+++ src/main/java/com/smilebear/swaggeruiexample/SwaggerUiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.smilebear.swaggeruiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerUiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerUiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smilebear/swaggeruiexample/SwaggerUiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilebear.swaggeruiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerUiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
